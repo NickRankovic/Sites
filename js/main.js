665ffbd7cbd7339cb66e7519c5ab0a2d72e79ebe
NickRankovic <101523907+NickRankovic@users.noreply.github.com>
2022-03-17 10:27:42 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -97,13 +97,40 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper('.portfolio__container', {
+    cssMode:true,
+    loop:true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive(){
+    const scrollY = window.pageXOffset
+
+    sections.forEach(current =>{
+        const sectionHeight =current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY>sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*='+sectionId+ ']').classList
+        }else{
+            document.querySelector('.nav__menu a[href*='+sectionId+ ']').classList
+        }
+    })
+}
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 
